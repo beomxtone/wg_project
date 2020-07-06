c623b647db11571e22d55c3db913684b7c97b771
beomxtone <soulstair@gmail.com>
2020-07-06 15:41:10 +0900

    Updates

---
+++ precache-manifest.a6ef0ec6931c4d6f78a355b9b72265ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3065969cbbe4593d23d3d59cd19759",
+    "url": "/wg_project/index.html"
+  },
+  {
+    "revision": "d82908a8579e0358658f",
+    "url": "/wg_project/static/css/main.711391f2.chunk.css"
+  },
+  {
+    "revision": "e1e2368a85d11253e498",
+    "url": "/wg_project/static/js/2.9a92aba6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wg_project/static/js/2.9a92aba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82908a8579e0358658f",
+    "url": "/wg_project/static/js/main.708ea152.chunk.js"
+  },
+  {
+    "revision": "50f1b65554f521a82044",
+    "url": "/wg_project/static/js/runtime-main.c0cd48be.js"
+  }
+]);
